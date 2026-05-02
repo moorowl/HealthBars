dae018e21d45f8e9a6546216d52f318ef7ff69ee
moorowl <moorowl@outlook.com>
2026-05-03 00:04:40 +0100

    Only override vanilla health colors if it's default

--- Scripts/OverrideHealthBarColor.cs
+++ Scripts/OverrideHealthBarColor.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using UnityEngine;
+
 // ReSharper disable InconsistentNaming
 
 namespace HealthBars {
@@ -7,10 +9,15 @@ namespace HealthBars {
 		[HarmonyPatch(typeof(HealthBar), "UpdateHealthBar")]
 		[HarmonyPrefix]
 		public static void HealthBar_UpdateHealthBar(HealthBar __instance, float value, int protectiveArmorValue, int maxProtectiveArmorValue) {
-			__instance.healthColor = Options.Instance.ColorHealth.Rgba;
-			
+			if (IsHealthColorDefault(__instance.healthColor))
+				__instance.healthColor = Options.Instance.ColorHealth.Rgba;
+
 			if (__instance.armorBar != null)
 				__instance.armorBar.color = Options.Instance.ColorShield.Rgba;
+		}
+
+		private static bool IsHealthColorDefault(Color color) {
+			return Mathf.RoundToInt(color.r * 255f) == 255 && Mathf.RoundToInt(color.g * 255f) == 61 && Mathf.RoundToInt(color.b * 255f) == 61;
 		}
 	}
 }

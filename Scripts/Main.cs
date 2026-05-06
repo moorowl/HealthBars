e7f42f1652c019492543624c31512fc05b717f1a
moorowl <moorowl@outlook.com>
2026-05-06 02:03:34 +0100

    Bump version to 1.4.1

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -8,7 +8,7 @@ using Object = UnityEngine.Object;
 
 namespace HealthBars {
 	public class Main : IMod {
-		public const string Version = "1.4";
+		public const string Version = "1.4.1";
 		public const string InternalName = "HealthBars";
 		public const string DisplayName = "Enemy Health Bars";
 		
